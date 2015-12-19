0f9dd4c15d8e86605434c28705dee7ade365085f
Josh Harris <joshuaharris51@gmail.com>
2015-12-19 20:15:52 +0000

    Rewrote project to use external webserver

--- install.php
+++ install.php
@@ -4,9 +4,6 @@ require __DIR__ . '/secrets.php';
 require __DIR__ . '/nginx.php';
 
 // Can be nginx or apache
-$engine = "nginx";
-$host = "stats.celant.co.uk";
-$nginx_vhosts = "/etc/nginx/sites-enabled/";
 
 $tables = [
 	'server' => "CREATE TABLE server(
@@ -64,29 +61,6 @@ foreach($tables as $key => $value) {
 }
 
 echo "All tables present and accounted for!\r\n";
-echo "Generating configuration files for specified http engine\r\n";
-
-if($engine == "nginx") {
-	// Generate NGINX vhost config
-	$file = __DIR__ . "/web/nginx/" . $host . ".conf";
-	$config = GenerateSiteConfig($host, __DIR__ . "/web/nginx/www");
-	file_put_contents($file, $config);
-	if(file_exists($nginx_vhosts . $host . ".conf")) {
-		if(is_link($nginx_vhosts . $host . ".conf")) {
-			unlink($nginx_vhosts . $host . ".conf");
-		} else {
-			die("$file exists but is not a symbolic link\r\n");
-		}
-	}
-	symlink($file, $nginx_vhosts . $host . ".conf");
-}
-elseif($engine == "apache") {
-	die("Apache is not yet supported.");
-}
-
-echo "Configs generated\r\n";
-echo "Starting up web server\r\n";
-
-if()
+echo "Installation complete!\r\n";
 
 ?>
