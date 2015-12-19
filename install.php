20369e170d673c113601189edd843ef57c1e91d2
Josh Harris <joshuaharris51@gmail.com>
2015-12-19 21:06:23 +0000

    Removed useless comment

--- install.php
+++ install.php
@@ -1,9 +1,6 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/secrets.php';
-require __DIR__ . '/nginx.php';
-
-// Can be nginx or apache
 
 $tables = [
 	'server' => "CREATE TABLE server(
